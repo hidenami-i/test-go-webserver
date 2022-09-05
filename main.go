73b4449a1a8ee8e8a34ebcf923505fd5d2da2f01
hidenami-i <shuhei.iwamoto.work@gmail.com>
2022-09-05 22:21:08 +0900

    fix

--- main.go
+++ main.go
@@ -3,29 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 	"webserver/config"
 )
 
 func main() {
-	//log.Println(os.Args)
-	//if len(os.Args) != 2 {
-	//	log.Printf("need port number\n")
-	//	os.Exit(1)
-	//}
-	//p := os.Args[1]
-	//log.Printf("port:%s", p)
-	//l, err := net.Listen("tcp", ":"+p)
-	//if err != nil {
-	//	return
-	//}
 	err := run(context.Background())
 	if err != nil {
 		log.Printf("failed to terminate server: %v", err)
@@ -52,33 +38,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	server := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg.Go(func() error {
-		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示すので異常ではないので除外
-		err := server.Serve(l)
-		if err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知を待機する
-	<-ctx.Done()
-	log.Printf("ctx done")
-	err = server.Shutdown(context.Background())
-	if err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	// Goメソッドで起動した別ゴルーチンの終了をまつ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
